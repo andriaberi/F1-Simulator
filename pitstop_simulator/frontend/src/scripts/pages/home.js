8d2bf77d30765fd1e8946e5fe5c871a62a916f6f
AndriaBeridze <andria24b@gmail.com>
2026-02-25 21:04:37 -0500

    Small changes

--- pitstop_simulator/frontend/src/scripts/pages/home.js
+++ pitstop_simulator/frontend/src/scripts/pages/home.js
@@ -4,10 +4,6 @@ function Home() {
     return (
         <>
            <Navbar pageName="home"/>
-            <div className="main">
-                <aside className="left"></aside>
-                <aside className="right"></aside>
-            </div>
         </>
     );
 }
